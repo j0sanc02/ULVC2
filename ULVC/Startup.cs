ab76942ecf5fdf0b821e0af07955b7f0752426c9
j0sanc02 <j0sanc02@louisville.edu>
2015-02-06 10:55:22 -0500

    Initial Commit

---
+++ ULVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ULVC.Startup))]
+namespace ULVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
